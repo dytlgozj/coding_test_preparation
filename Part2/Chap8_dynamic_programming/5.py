58a79666a4f062ad8c06aef6209e2e4ec3570468
dytlgozj <dytlgozj@naver.com>
2022-02-15 23:37:47 +0900

    2022_2_15 커밋

--- Part2/Chap8_dynamic_programming/5.py
+++ Part2/Chap8_dynamic_programming/5.py
@@ -1,6 +1,21 @@
 n, m = map(int, input().split())
 coins = []
+dp = [10001] * (m + 1)
 
 for _ in range(n):
     coins.append(int(input()))
 
+coins.sort()
+
+dp[0] = 0
+
+for i in range(m + 1):
+    if dp[i] != 10001:
+        for coin in coins:
+            if i + coin <= m:
+                dp[i + coin] = min(dp[i + coin], dp[i] + 1)
+
+if dp[m] == 10001:
+    print(-1)
+else:
+    print(dp[m])
